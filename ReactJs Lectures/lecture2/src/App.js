671b7783000d9e7dd1208b0e627426eaca9fa5fc
Vaghaniharshh <hvaghani89@gmail.com>
2025-12-24 15:48:06 +0530

    24-12-25

---
+++ ReactJs Lectures/lecture2/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Buttons from './Component/Button';
+import DropDownMenu from './Component/DropDownMenu';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello ReactJs</h1>
+      <Buttons />
+      <DropDownMenu />
+    </div>
+  );
+}
+export default App;
+
---
+++ ReactJs Lectures/lecture2/src/Component/Button.js
@@ -0,0 +1,11 @@
+import Button from 'react-bootstrap/Button';
+
+const Buttons = () => {
+  return (
+    <div>
+      <Button variant='primary'>Submit</Button>
+    </div>
+  )
+}
+
+export default Buttons
